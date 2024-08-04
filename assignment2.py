c19fe60a2b844c9c90fb2728bc35687d83ed0670
Nguyen Nhat Huy <122243684+NNHuy1212@users.noreply.github.com>
2024-08-04 13:21:13 +0700

    Update assignment2.py
    Added try again functionality after an error message.

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,22 @@
 def split_and_sort(nums):
     # check if input list length is less than or equal to 20
     if len(nums) > 20:
-        return "Error: Input list should not contain more than 20 integers."
+        print("Error: Input list should not contain more than 20 integers.")
+        print("-----------------------------------------------------------")
+        return None
 
-    # check if 0 is in the input list
+    # check if input list contains any non-positive integers
+    if any(num < 0 for num in nums):
+        print("Error: The input list should only contain positive integers.")
+        print("-----------------------------------------------------------")
+        return None
+    
+     # check if 0 is in the input list
     if 0 in nums:
-        return "Error: The number 0 is not a valid input."
+        print ("Error: The number 0 is not a valid input.")
+        print("-----------------------------------------------------------")
+        return None
+
 
     nums = set(nums)
     # filter odd and even numbers into two separate lists
@@ -18,16 +29,21 @@ def split_and_sort(nums):
 
     return odd_nums, even_nums
 
-# Get user input for the list of numbers
-user_input = input("Enter a list of numbers separated by spaces: ")
-nums = [int(num) for num in user_input.split()]
+while True:
+    user_input = input("Enter a list of positive integers separated by spaces: ")
+    try:
+        nums = [int(num) for num in user_input.split()]
+        result = split_and_sort(nums)
+    except ValueError:
+        print("Error: The input should only contain positive integers.")
+        print("-----------------------------------------------------------")
+        continue
 
-result = split_and_sort(nums)
-
-if isinstance(result, str):
-    print(result)
-else:
-    odd_nums, even_nums = result
-    print("Input:", nums)
-    print("Odd numbers:", odd_nums)
-    print("Even numbers:", even_nums)
+    if result is None:
+        continue
+    else:
+        odd_nums, even_nums = result
+        print("Input:", nums)
+        print("Odd numbers:", odd_nums)
+        print("Even numbers:", even_nums)
+        break
